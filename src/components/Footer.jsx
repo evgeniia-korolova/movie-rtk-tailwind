1b07a4ea080448efa35af9c40a9e628a35bd1848
evgeniia-korolova <jkod@ukr.net>
2024-12-23 20:51:57 +0200

    remove doubbled link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,7 @@ const Footer = () => {
 		<footer className='text-center  bg-black bg-opacity-70 z-20 text-newtural-300 py-3 lg:fixed bottom-0 w-full '>
 			<div className='flex gap-4 items-center justify-center'>
 				<Link to='/'>About</Link>
-				<Link to='/'>Contact</Link>
-				<Link>About</Link>
+				<Link to='/'>Contact</Link>				
 			</div>
 			<p className='text-sm py-2 pt-5'>
 				Created by Evgeniia Korolova with the help of Amit's tutorial
